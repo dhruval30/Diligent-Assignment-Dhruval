160f9280b35858886c2208c7ca8dcc7c81a3e5ac
Dhruval Padia <padiadhruval@gmail.com>
2026-01-21 13:59:46 +0530

    setting up for deployment

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 // Use the chat routes
 app.use('/api', chatRoutes);
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Jarvis Backend is running');
